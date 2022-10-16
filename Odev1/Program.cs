65c1a089c6d90586544ecb226a6f851b1336604a
Sercanakinci <sercan_yasin_1907@hotmail.com>
2022-10-17 01:25:43 +0300

    Add project files.

---
+++ Odev1/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Odev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Öğrenci Vize Final Notu Hesaplama");
+
+            // Console'^dan girilen vize ve final notları ile ortalamayı hesaplayan bu ortalamaya göre harfnotunu bulup ekrana ortalama ve harfnotunu yazan prog. yazınız.. tanımladığım değişkenleri kullanınız... 
+
+
+            float vize, final, ort = 0;
+
+            // vize notu isteyiniz
+
+            // final notu isteyiniz
+
+            // ortalama hesaplayınız
+            // ortalam vize notunun %40 finalnotunun %60 formülü ile hesaplanır. Formül (vizenotu * 0.4) + (finalnotu * 0.6)
+
+            Console.Write("Vize notunu giriniz: ");
+            vize = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Final notunu giriniz: ");
+            final = Convert.ToInt32(Console.ReadLine());
+
+            ort = Convert.ToInt32(vize * 0.4 + final * 0.6);
+
+            Console.WriteLine("Ortalamanız = {0}", ort);
+
+            if (ort < 50 || final < 50)
+                Console.WriteLine("Kaldınız.");
+
+            else
+                Console.WriteLine("Geçtiniz.");
+
+            Console.ReadKey();
+
+
+            string harfNotu;
+
+
+
+            // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+
+            if (ort >= 85 && ort <= 100)
+            {
+                Console.WriteLine($"Ortalamanız ={ort} harfNotunuz= AA");
+            }
+
+            else if (ort >= 75 && ort <= 84)
+            {
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = BA");
+            }
+
+            else if (ort >= 65 && ort <= 74)
+            {
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = BB");
+            }
+
+            else if (ort >= 50 && ort <= 64)
+            {
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = CC");
+            }
+
+            else if (ort >= 25 && ort <= 49)
+            {
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = DC");
+            }
+
+            else if (ort <= 25)
+            {
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = FF");
+            }
+
+            // todo: bütün  istenilenleri switch-case ve if karar mekanizmaları kullanarak yapınız
+
+
+            Console.WriteLine("Kapatmak için bir tuşa basınız");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Odev2/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace Odev2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console'dan alınan ay'ın mevsimini yazdıran prorgramı yapınız.
+            Console.WriteLine("Lütfen ay giriniz");
+            string ay = Console.ReadLine();
+
+
+
+
+            // eğer ay;  aralık-ocak-şubat      girilirse ekrana KIŞ 
+            // mart-nisan-mayıs       girilirse ekrana İLK BAHAR
+            // haziran-temmuz-ağustos girilirse ekrana YAZ 
+            // eylük-ekim-kasım       girilirse ekrana SON BAHAR
+            // mesajlarını veren programı yazınız.. Girilen ay için kaçıncı ay olduunu yazınız ekrana n. Ay şeklinde yazınız..
+
+            // örnek;
+            // klavyeden girilen ocak ayı için çıktı aşağıdaki gibi olmalıdır;
+            // KIŞ
+            // 1. Ay
+
+
+            // todo: bütün  istenilenleri switch-case ve if karar mekanizmaları kullanarak yapınız
+
+
+            switch (ay)
+            {
+                case ("ocak"):
+                    Console.WriteLine("KIŞ");
+                    Console.WriteLine("1.AY");
+
+                    break;
+
+                case ("şubat"):
+                    Console.WriteLine("KIŞ");
+                    Console.WriteLine("2.AY");
+
+                    break;
+
+                case ("mart"):
+                    Console.WriteLine("İLKBAHAR");
+                    Console.WriteLine("3.AY");
+
+                    break;
+
+                case ("nisan"):
+                    Console.WriteLine("İLKBAHAR");
+                    Console.WriteLine("4.AY");
+
+                    break;
+
+                case ("mayıs"):
+                    Console.WriteLine("İLKBAHAR");
+                    Console.WriteLine("5.AY");
+
+                    break;
+
+                case ("haziran"):
+                    Console.WriteLine("YAZ");
+                    Console.WriteLine("6.AY");
+
+                    break;
+
+                case ("temmuz"):
+                    Console.WriteLine("YAZ");
+                    Console.WriteLine("7.AY");
+
+                    break;
+
+                case ("agustos"):
+                    Console.WriteLine("YAZ");
+                    Console.WriteLine("8.AY");
+
+                    break;
+
+                case ("eylül"):
+                    Console.WriteLine("SONBAHAR");
+                    Console.WriteLine("9.AY");
+
+                    break;
+
+                case ("ekim"):
+                    Console.WriteLine("SONBAHAR");
+                    Console.WriteLine("10.AY");
+
+                    break;
+
+                case ("kasım"):
+                    Console.WriteLine("SONBAHAR");
+                    Console.WriteLine("11.AY");
+
+                    break;
+
+                case ("aralık"):
+                    Console.WriteLine("KIŞ");
+                    Console.WriteLine("12.AY");
+
+                    break;
+
+                default:
+                    Console.WriteLine("!! ÖYLE BİR AY YOK !!");
+                    break;
+
+
+
+
+
+            }
+            Console.WriteLine("Kapatmak için bir tuşa basınız");
+            Console.ReadLine();
+
+
+        }
+    }
+}
